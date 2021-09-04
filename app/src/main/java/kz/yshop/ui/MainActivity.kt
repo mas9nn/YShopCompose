f10e6ece0c2b280e79506b1988ade9b837b10a51
Almas <almasnurlanov16@gmail.com>
2021-09-04 19:57:49 +0600

    Drawer added

--- app/src/main/java/kz/yshop/ui/MainActivity.kt
+++ app/src/main/java/kz/yshop/ui/MainActivity.kt
@@ -6,10 +6,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.R
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import coil.annotation.ExperimentalCoilApi
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kz.yshop.ui.components.Drawer
 import kz.yshop.ui.main.MainScreenViewModel
 import kz.yshop.ui.theme.YShopTheme
 import kz.yshop.ui.util.Navigation
@@ -26,6 +39,7 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var sharedPreferences: SharedPreferences
 
+    @ExperimentalCoilApi
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +48,13 @@ class MainActivity : ComponentActivity() {
             YShopTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    val drawerState = rememberDrawerState(DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+                    val openDrawer = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }
                     val navController = rememberAnimatedNavController()
                     viewModel.error.observe(this, { action ->
                         Timber.wtf("observer $action")
@@ -46,13 +67,43 @@ class MainActivity : ComponentActivity() {
                             action
                         }.show()
                     })
-                    if (viewModel.shopInfo.value != null) {
-                        Navigation(navController = navController, viewModel = viewModel)
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "Home"
+                                )
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { openDrawer }) {
+                                    Icon(Icons.Filled.Menu, contentDescription = "")
+                                }
+                            },
+                            backgroundColor = MaterialTheme.colors.primaryVariant
+                        )
+                        ModalDrawer(
+                            drawerState = drawerState,
+                            gesturesEnabled = true,
+                            drawerContent = {
+                                Drawer(
+                                    onDestinationClicked = { route ->
+                                        scope.launch {
+                                            drawerState.close()
+                                        }
+                                    }
+                                )
+                            }
+                        ) {
+                            if (viewModel.shopInfo.value != null) {
+                                Navigation(navController = navController, viewModel = viewModel)
+                            }
+                        }
                     }
                 }
             }
         }
     }
+
 
 //    @Composable
 //    fun erroSnackBar(action: () -> Unit) {
---
+++ app/src/main/java/kz/yshop/ui/components/Drawer.kt
@@ -0,0 +1,38 @@
+package kz.yshop.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kz.yshop.R
+
+@Composable
+fun Drawer(
+    modifier: Modifier = Modifier,
+    onDestinationClicked: (route: String) -> Unit
+) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, top = 48.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = "App icon"
+        )
+//        screens.forEach { screen ->
+//            Spacer(Modifier.height(24.dp))
+//            Text(
+//                text = screen.title,
+//                style = MaterialTheme.typography.h4,
+//                modifier = Modifier.clickable {
+//                    onDestinationClicked(screen.route)
+//                }
+//            )
+//        }
+    }
+}
